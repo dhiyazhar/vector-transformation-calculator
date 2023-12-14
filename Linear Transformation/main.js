b7a0376f5428ea25bbff95f26a310e5a5916ac31
Mpac <126958166+Chanie-ago@users.noreply.github.com>
2023-12-14 16:05:03 +0700

    Update main.js

--- Linear Transformation/main.js
+++ Linear Transformation/main.js
@@ -29,8 +29,23 @@ function hasiluuT(vector){
     return newArray;
 }
 
-function identityMatrix(){
+function identityMatrix(vector){
+    let matrix = [];
+    for (let i = 0; i < vector.length; i++) {
+        let temp = [];
 
+        for (let j = 0; j < vector.length; j++) {
+            if (i == j) {
+                temp.push(1);
+            }
+            else {
+                temp.push(0);
+  
+            }
+        }
+        matrix.push(temp);
+    }
+    return matrix;
 }
 
 function projection(vector) {
@@ -56,52 +71,32 @@ function projection(vector) {
 }
 
 function reflection(vector){
-    let array = hasiluuT (vector);
-    for(var i = 0; i < vector.length; i++ ){
-        for(var j = 0; j < vector.length; j++){
-            
-            const resultReflection = 2 * array[i][j] / panjang(vector);
-            array [i][j] = resultReflection.toFixed(2);
-
-        }
-    }
-
+    let array = hasiluuT(vector);
+    let length = panjang(vector);
+    let matrix_identity = identityMatrix(vector);
+ 
     let result = [];
-    for(var i = 0; i < vector.length; i++ ){
-        let temp = "[";
-        temp += array[i].join(", ");
-        temp += "]";
-        result.push (temp);
+    for (var i = 0; i < vector.length; i++) {
+        let temp = [];
+        for (var j = 0; j < vector.length; j++) {
+            const resultReflection = (2 * array[i][j] / length) - matrix_identity[i][j];
+            temp.push(resultReflection.toFixed(2));
+        }
+        result.push("[" + temp.join(", ") + "]");
     }
-
+ 
     return result.join(", ");
 }
-
-
+   
 function calculateResults() {
     const vectorV = document.getElementById('vectorV').value;
     const v = vectorV.split(',').map(Number);
     console.log(panjang(v));
     console.log(hasiluuT(v));
-    console.log(reflection(v));
+    console.log(identityMatrix(v));
     console.log("aku " + "suka" + " azhar");
 
     document.getElementById('projection-result').innerHTML = `Projection Matrix: [ ${projection(v)} ]`;
 
-    // Reflection Calculation
-    const r11 = (v[0] * v[0])  ;
-    const r12 = (v[0] * v[1]) ;
-    const r21 = (v[1] * v[0]) ;
-    const r22 = (v[1] * v[1]) ;
-
-    document.getElementById('reflection-result').innerHTML = `Projection Matrix: [ ${reflection(v)} ]`;
-
-    // Rotation Calculation
-    const angle = Math.PI / 2; // 90 degrees for simplicity
-    const r11_rot = Math.cos(angle);
-    const r12_rot = -Math.sin(angle);
-    const r21_rot = Math.sin(angle);
-    const r22_rot = Math.cos(angle);
-
-    document.getElementById('rotation-result').innerHTML = `Rotation Matrix: [ [${r11_rot.toFixed(2)}, ${r12_rot.toFixed(2)}], [${r21_rot.toFixed(2)}, ${r22_rot.toFixed(2)}] ]`;
+    document.getElementById('reflection-result').innerHTML = `Reflection Matrix: [ ${reflection(v)} ]`;
 }
